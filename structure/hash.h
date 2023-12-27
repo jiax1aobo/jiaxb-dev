3854df313eb3313d2e927f3ce58f1c2d6400b2bb
jacob <jiax1aobo@163.com>
2023-12-27 15:37:07 +0000

    added hash function

---
+++ structure/hash.h
@@ -0,0 +1,19 @@
+#ifndef _HASH_H_
+#define _HASH_H_ 1
+
+#include <stdint.h>
+#include <stdlib.h>
+
+/*
+ * murmurhash3
+ * from github
+ */
+uint32_t byte_hash_code(const char *key, uint32_t len, uint32_t seed);
+
+/*
+ * Thomas Wang's 32 bit Mix Function
+ * from redis
+ */
+uint32_t integer_hash_code(uint32_t key);
+
+#endif /* _HASH_H_ */
--- structure/hash_table.h
+++ structure/hash_table.h
@@ -2,17 +2,32 @@
 #define _HASH_TABLE_H_ 1
 
 #include "dlist.h"
+#include <stdint.h>
 
-typedef struct _hash_table_module
+typedef struct _hash_elem
 {
-    
-} hash_table_class;
+    void *key;
+    list_node link;
+} hash_elem;
 
+typedef struct _hash_elem_list
+{
+    dlist handle;
+    uint32_t count;
+} hash_elem_list;
+
+typedef struct _bucket_list
+{
+    hash_elem_list elem_list;
+    bucket_list *next;
+} bucket_list;
 
 typedef struct _hash_table
 {
     int capacity;
-    dlist buckets;
+    bucket_list hash_bucket;
 } hash_table;
+
+void *init_hash_table()
 
 #endif /* _HASH_TABLE_H_ */
